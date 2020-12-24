99109bc793003cc14922cdf82a1c1b1cf1ec76de
gbarros1994 <55881896+gbarros1994@users.noreply.github.com>
2020-12-24 01:28:09 -0300

    metodos CRUD

--- src/app.js
+++ src/app.js
@@ -1,13 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+
 
 const app = express();
-
 const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
         title: 'node',
         version: '01'
+    });
+});
+
+const create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body);
+});
+
+const create = router.put('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(201).send({
+        id: id,
+        item: req.body
     });
 });
 
