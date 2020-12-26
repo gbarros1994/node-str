5b73c24f66194d1dedff39c9042ea65fe774a647
gbarros1994 <55881896+gbarros1994@users.noreply.github.com>
2020-12-26 13:34:59 -0300

    separando rotas

--- src/app.js
+++ src/app.js
@@ -1,33 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-
-
 const app = express();
 const router = express.Router();
+
+//CARREGA AS ROTAS 
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: 'node',
-        version: '01'
-    });
-});
-
-const create = router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
-
-const create = router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(201).send({
-        id: id,
-        item: req.body
-    });
-});
-
-app.use('/', route);
+app.use('/', indexRoute);
+app.use('/products', productRoute);
 
 module.exports = app;
