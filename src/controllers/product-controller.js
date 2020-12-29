0683d889e7a64ab065a118bfcce5ae41d07d586d
gbarros1994 <55881896+gbarros1994@users.noreply.github.com>
2020-12-29 02:05:36 -0300

    atualizando produtos

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -31,7 +31,24 @@ exports.post = (req, res, next) => {
 };
 
 exports.put = (req, res, next) => {
-    res.status(201).send(req.body);
+    const id = req.params.id;
+    Product
+    .findByIdAndUpdate(req.params.id, {
+        $set: {
+            title: req.body.title,
+            description: req.body.description,
+            price: req.body.price
+        }
+    }).then(x => {
+        res.status(200).send({
+            message: 'Produto atualizado com sucesso!'
+        });
+    }).catch(e => {
+        res.status(400).send({
+            message: 'Falha ao atualizar produto',
+            data: e
+        });
+    });
 };
 
 exports.delete = (req, res, next) => {
