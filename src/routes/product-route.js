19eade548487b12b9452e1644f8d4bffdfc35b87
gbarros1994 <55881896+gbarros1994@users.noreply.github.com>
2021-01-07 01:31:17 -0300

    auth service

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -3,10 +3,11 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/product-controller');
+const authService = require('../services/auth-service');
 
 router.get('/', controller.get);
 router.get('/', controller.getBySlug);
-router.post('/', controller.post);
+router.post('/', authService.authorize ,controller.post);
 router.put('/:id', controller.delete);
 router.delete('/', controller.put);
 
