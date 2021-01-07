1d70b7fa13865abe75b67703b55a67c3e3b3f6c1
gbarros1994 <55881896+gbarros1994@users.noreply.github.com>
2021-01-07 01:40:51 -0300

    token

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -8,7 +8,7 @@ const authService = require('../services/auth-service');
 router.get('/', controller.get);
 router.get('/', controller.getBySlug);
 router.post('/', authService.authorize ,controller.post);
-router.put('/:id', controller.delete);
-router.delete('/', controller.put);
+router.put('/:id',  authService.authorize ,controller.delete);
+router.delete('/',  authService.authorize ,controller.put);
 
 module.exports = router;
