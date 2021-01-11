9025afeecee80c21b1792fbb3c78abaf28323a7f
gbarros1994 <55881896+gbarros1994@users.noreply.github.com>
2021-01-11 00:52:00 -0300

    autorização

--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -24,7 +24,8 @@ exports.post = async(req, res, next) => {
         await repository.create({
             name: req.body.name,
             email: req.body.email,
-            password: md5(req.body.password + global.SALT_KEY)
+            password: md5(req.body.password + global.SALT_KEY),
+            roles: ["user"]
         });
 
         emailService.send(
@@ -62,7 +63,8 @@ exports.authenticate = async(req, res, next) => {
         const token = await authService.generateToken({
             id: customer._id,
             email: customer.email, 
-            name: customer.name
+            name: customer.name,
+            roles: customer.roles,
         })
 
         res.status(201).send({
@@ -98,7 +100,8 @@ exports.refreshToken = async(req, res, next) => {
         const tokenData = await authService.generateToken({
             id: customer._id,
             email: customer.email, 
-            name: customer.name
+            name: customer.name,
+            roles: customer.roles,
         })
 
         res.status(201).send({
--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -7,8 +7,8 @@ const authService = require('../services/auth-service');
 
 router.get('/', controller.get);
 router.get('/', controller.getBySlug);
-router.post('/', authService.authorize ,controller.post);
-router.put('/:id',  authService.authorize ,controller.delete);
-router.delete('/',  authService.authorize ,controller.put);
+router.post('/', authService.isAdmin ,controller.post);
+router.put('/:id',  authService.isAdmin ,controller.delete);
+router.delete('/',  authService.isAdmin ,controller.put);
 
 module.exports = router;
