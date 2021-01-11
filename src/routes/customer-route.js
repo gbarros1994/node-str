e138f540a05fe4c7632fca02c48b947e69a9cfad
gbarros1994 <55881896+gbarros1994@users.noreply.github.com>
2021-01-10 22:55:45 -0300

    refesh token

--- src/routes/customer-route.js
+++ src/routes/customer-route.js
@@ -2,9 +2,11 @@
 
 const express = require('express');
 const router = express.Router();
+const authService = require('../services/auth-service');
 const controller = require('../controllers/customer-controller');
 
 router.post('/', controller.post);
 router.post('/authenticate', controller.authenticate);
+router.post('/refresh-token', authService.authorize, controller.refreshToken);
 
 module.exports = router;
